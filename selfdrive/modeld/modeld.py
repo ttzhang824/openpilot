7d9a7bd3c34eafb73c857345f9fe83d065015bcd
Bruce Wayne <harald.the.engineer@gmail.com>
2024-08-16 14:11:29 -0700

    Get curv from plan

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -262,7 +262,7 @@ def main(demo=False):
       drivingdata_send = messaging.new_message('drivingModelData')
       posenet_send = messaging.new_message('cameraOdometry')
       fill_model_msg(drivingdata_send, modelv2_send, model_output, publish_state, meta_main.frame_id, meta_extra.frame_id, frame_id,
-                     frame_drop_ratio, meta_main.timestamp_eof, model_execution_time, live_calib_seen)
+                     frame_drop_ratio, meta_main.timestamp_eof, model_execution_time, sm["carState"].vEgo, steer_delay, live_calib_seen)
 
       desire_state = modelv2_send.modelV2.meta.desireState
       l_lane_change_prob = desire_state[log.Desire.laneChangeLeft]
