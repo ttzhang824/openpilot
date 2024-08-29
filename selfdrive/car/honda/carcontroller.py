8451c0fcba6ea9c8eb7082ebc7cd349c97089086
ttzhang824 <tiantian.zhang@gmail.com>
2024-08-29 13:20:43 -0500

    Fix OP unavailable issue

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -159,7 +159,7 @@ class CarController:
     apply_steer = int(interp(-limited_steer * self.params.STEER_MAX,
                              self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
     if (CS.CP.carFingerprint in SERIAL_STEERING):
-      apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
+      apply_steer = apply_driver_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
       self.apply_steer_last = apply_steer
       if apply_steer > 229 and False:
         apply_steer_orig = apply_steer
