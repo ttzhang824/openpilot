80949577114cf3d9c0c750fdd0a54dfc8b3eecbf
ttzhang824 <tiantian.zhang@gmail.com>
2024-08-29 13:18:29 -0500

    Fix OP unavailable issue

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -4,7 +4,7 @@ from cereal import car
 from openpilot.common.numpy_fast import clip, interp
 from openpilot.common.realtime import DT_CTRL
 from opendbc.can.packer import CANPacker
-from openpilot.selfdrive.car import create_gas_interceptor_command, apply_std_steer_torque_limits
+from openpilot.selfdrive.car import create_gas_interceptor_command, apply_driver_steer_torque_limits
 from openpilot.selfdrive.car.honda import hondacan
 from openpilot.selfdrive.car.honda.values import CruiseButtons, VISUAL_HUD, HONDA_BOSCH, HONDA_BOSCH_RADARLESS, HONDA_NIDEC_ALT_PCM_ACCEL, CarControllerParams, SERIAL_STEERING, LKAS_LIMITS
 from openpilot.selfdrive.controls.lib.drive_helpers import rate_limit
